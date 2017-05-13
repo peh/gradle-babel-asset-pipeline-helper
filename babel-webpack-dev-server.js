d6ea57d21a986c905b28522b6c424f694ca1c6a3
Philipp Eschenbach <philipp@uberall.com>
2017-05-13 11:45:51 +0200

    fix cors issue

--- babel-webpack-dev-server.js
+++ babel-webpack-dev-server.js
@@ -32,6 +32,7 @@ new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true,
+  headers: { 'Access-Control-Allow-Origin': '*' },
   stats: 'errors-only'
 }).listen(listenPort, 'localhost', function (err) {
   if (err) {
